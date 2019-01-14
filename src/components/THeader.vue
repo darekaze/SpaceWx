b322e622a3a6a74ada7b2b3b002e8911f144f0c9
darekaze <darekaze@gmail.com>
2019-01-14 17:15:43 +0800

    Refactor Header and add temp carousel

--- src/components/THeader.vue
+++ src/components/THeader.vue
@@ -1,23 +1,36 @@
 <template>
   <v-toolbar
     flat dark fixed
-    height="56px"
+    height="54px"
     color="#000000a8">
-    <v-toolbar-title class="headline">
+    <v-toolbar-title class="headline font-weight-light">
       <span>Space Weather</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-xs-only">
-      <!-- FIXME: Use vfor -->
-      <v-btn flat class="text-capitalize">Topics</v-btn>
-      <v-btn flat class="text-capitalize">Impacts</v-btn>
-      <v-btn flat class="text-capitalize">Events</v-btn>
+    <v-toolbar-items
+      class="hidden-xs-only"
+      v-for="(item, index) in toolbarItems" :key="index">
+      <v-btn
+        class="text-capitalize"
+        :to="`/${item}`" flat>
+        {{ item }}
+      </v-btn>
     </v-toolbar-items>
+    <v-toolbar-items
+      class="hidden-sm-and-up">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+    </v-toolbar-items>
+    <!-- TODO: add hamburger for mobile screen -->
   </v-toolbar>
 </template>
 
 <script>
 export default {
-
+  name: 'main-header',
+  data() {
+    return {
+      toolbarItems: ['topics', 'impacts', 'events'],
+    };
+  },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <HelloWorld />
-    <HelloWorld />
-    <HelloWorld />
+    <v-carousel hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+  <hello-world/>
   </div>
 </template>
 
@@ -10,6 +15,21 @@
 export default {
   components: {
     HelloWorld: () => import('@/components/HelloWorld.vue'),
+  },
+  data () {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        }
+      ]
+    }
   },
 };
 </script>
