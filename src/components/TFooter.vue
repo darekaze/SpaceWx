67355701cd626d286096e3c6a314af7d774278f2
darekaze <darekaze@gmail.com>
2019-01-23 10:13:51 +0800

    Finish footer

--- src/components/TFooter.vue
+++ src/components/TFooter.vue
@@ -10,37 +10,52 @@
         px-5 pt-4 pb-3 xs12>
         <div class="title">Related Links</div>
       </v-flex>
-
       <v-flex
         px-5 py-2 xs12 sm6
+        class="t-hov"
         v-for="(item, index) in links" :key="index">
         <v-icon small>navigate_next</v-icon>
-        {{ item }}
+        <a
+          class="c-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          :href="item.url" >
+          {{ item.name }}
+        </a>
+      </v-flex>
+      <v-flex
+        py-3 mx-3 xs12
+        text-xs-center>
+        <v-divider/>
+        <div class="pt-3">
+          &copy;2019 — <strong>Hong Kong Observatory</strong>
+        </div>
       </v-flex>
 
-      <v-flex
-        py-3 xs12
-        text-xs-center>
-        &copy;2019 — <strong>Hong Kong Observatory</strong>
-      </v-flex>
     </v-layout>
   </v-footer>
 </template>
 
 <script>
+import links from '@/assets/context/links.json';
+
 export default {
   data() {
     return {
-      links: [
-        'Solar Activity Prediction Center',
-        'European Space Weather Portal',
-        'UK Met Office',
-        'Space Environment Prediction Center',
-        'Space Weather Prediction Center (SWPC)',
-        'Space Weather Action Center (NASA)',
-        'Hong Kong Space Museum',
-      ],
+      links,
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.c-link {
+  text-decoration: none;
+  color: #fff;
+  transition:all 0.2s ease;
+
+  &:hover {
+    color: #b9b9b9;
+  }
+}
+</style>
