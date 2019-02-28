14dd67bb0847706af5ee5385ac165336ed5ed592
darekaze <darekaze@gmail.com>
2019-02-28 12:32:59 +0800

    Style cannot be removed, so add it back

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/vuetify';
+import './stylus/main.styl';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
