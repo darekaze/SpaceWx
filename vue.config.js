48e0b97b1f493258892967e32cba32791c20745c
darekaze <darekaze@gmail.com>
2019-03-11 11:20:34 +0800

    Add loader and i18n

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-unused-vars */
+const md = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: true,
+})
+  .use(require('markdown-it-link-attributes'), {
+    attrs: {
+      target: '_blank',
+      rel: 'noopener noreferrer nofollow',
+    },
+  })
+  .use(require('markdown-it-ins'))
+  .use(require('markdown-it-sup'));
+
 const subDirName = 'r2dev/vueswx';
 
 module.exports = {
@@ -5,4 +20,25 @@ module.exports = {
     ? `/${subDirName}/`
     : '/',
   outputDir: `dist/${subDirName}`,
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true,
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('markdown')
+      .test(/\.md$/)
+      .use('frontmatter-markdown-loader')
+      .loader('frontmatter-markdown-loader')
+      .tap(options => ({
+        vue: {
+          root: 'markdown-body',
+        },
+        markdown: body => md.render(body),
+      }));
+  },
 };
