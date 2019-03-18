afa9c270e413d92dfaff7aad832544937a79abce
darekaze <darekaze@gmail.com>
2019-03-18 10:33:51 +0800

    change path name

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ const md = require('markdown-it')({
   .use(require('markdown-it-ins'))
   .use(require('markdown-it-sup'));
 
-const subDirName = 'education/spacewx';
+const subDirName = 'V2/education/spacewx';
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
